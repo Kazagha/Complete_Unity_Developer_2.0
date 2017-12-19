e9cc734c71fb6ae42f69a36417f4642c31391346
Unknown <chrisg.local@gmail.com>
2017-12-19 21:33:41 +1000

    03_Project_Boost: User input and thrust
    Begin to handle user input
    Add basic thrust

---
+++ 03_Project_Boost/Assets/Player Ship/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody ridgidBody;
+
+	// Use this for initialization
+	void Start () {
+        ridgidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    // Process user input
+    private void ProcessInput()
+    {
+        // Check for thrust
+        if(Input.GetKey(KeyCode.Space))
+        {
+            print("SPACE!!!!");
+            //ridgidBody.AddRelativeForce(new Vector3(0, 1, 0));
+            ridgidBody.AddRelativeForce(Vector3.up);
+        } 
+
+        // Check for rotation
+        if(Input.GetKey(KeyCode.A))
+        {
+            print("Turning Left");
+        } else if (Input.GetKey(KeyCode.D))
+        {
+            print("Turning Right");
+        }
+    }
+}
