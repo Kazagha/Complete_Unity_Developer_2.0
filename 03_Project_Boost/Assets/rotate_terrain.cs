a36245261fc516245e85a744d0e4db9eee033235
Unknown <chrisg.local@gmail.com>
2017-12-26 12:22:00 +1000

    03_Project_Boost: Moving terrain
    Terrain that rotates around a centre point

---
+++ 03_Project_Boost/Assets/rotate_terrain.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate_terrain : MonoBehaviour {
+
+    [SerializeField] float turn_speed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotate();
+	}
+
+    void rotate()
+    {
+        float rotation_this_frame = turn_speed * Time.deltaTime;
+        transform.Rotate(Vector3.forward * rotation_this_frame);
+    }
+}
