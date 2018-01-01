7cda9666977e0b4a7a6a7df0f8902a94def0fd1f
Kazagha <chrisg.local@gmail.com>
2018-01-01 13:45:25 +1000

    03_Project_Boost: Oscillator script
    Manual movement of terrain in the game

--- 03_Project_Boost/Assets/Player Ship/Oscillator.cs
+++ 03_Project_Boost/Assets/Player Ship/Oscillator.cs
@@ -6,14 +6,26 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour {
 
     [SerializeField] Vector3 movementVector;
+    [SerializeField] float movementPercent; 
 
-	// Use this for initialization
-	void Start () {
-		
+    // todo remove from the inspector later
+    [Range(0, 1)]
+    [SerializeField]
+    float movementFactor; // 0 for not moved, 1 for fully moved
+
+    private Vector3 startingPos;
+
+    // Use this for initialization
+    void Start () {
+        // Record the starting position of the object
+        startingPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        // Calculate the offset
+        Vector3 offset = movementVector * movementFactor;
+        // Move the object to the value of the offset
+        transform.position = startingPos + offset;
 	}
 }
