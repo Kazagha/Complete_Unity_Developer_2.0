deb48486e08a5a63861f2f8cbe1a25b2a4333158
Kazagha <chrisg.local@gmail.com>
2018-01-01 14:13:17 +1000

    03_Project_Boost: Oscillator updates
    Automatically move the terrain

--- 03_Project_Boost/Assets/Player Ship/Oscillator.cs
+++ 03_Project_Boost/Assets/Player Ship/Oscillator.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour {
 
     [SerializeField] Vector3 movementVector;
-    [SerializeField] float movementPercent; 
+    [SerializeField] float movementSpeed; 
 
     // todo remove from the inspector later
     [Range(0, 1)]
@@ -22,10 +22,33 @@ public class Oscillator : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        if (movementFactor > 1)
+        {
+            movementSpeed = -movementSpeed;
+        }
+        else if (movementFactor < 0)
+        {
+            movementSpeed = -movementSpeed;
+        }
+
+        // Calculate movement factor
+        movementFactor = calcMovementFactor(movementSpeed);
+        // Set the transform
+        offsetTransform();
+    }
+
+    private void offsetTransform()
+    {
         // Calculate the offset
         Vector3 offset = movementVector * movementFactor;
         // Move the object to the value of the offset
         transform.position = startingPos + offset;
-	}
+    }
+
+    private float calcMovementFactor(float speed)
+    {
+        return movementFactor + (speed * Time.deltaTime);
+    }
 }
