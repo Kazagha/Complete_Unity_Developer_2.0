2594530deec2b726f24e8560b98092c0e8a8aa19
Kazagha <chrisg.local@gmail.com>
2018-04-14 17:44:12 +1000

    04_Argon_Assault: Scene Controller
    Script loading the next scene

---
+++ 04_Argon_Assault/Assets/Scripts/SceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneController : MonoBehaviour {
+
+    [SerializeField] float levelLoadDelay;
+
+    // Use this for initialization
+    void Start () {
+        Invoke("LoadNextLevel", levelLoadDelay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        		
+	}
+
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // Increment one to the next scene
+        // Use the module operator (division remainder) to return to index 0
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
