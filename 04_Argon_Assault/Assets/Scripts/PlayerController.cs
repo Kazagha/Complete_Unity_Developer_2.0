bb19490ea2c8d92fe26af792e25e0e7ffac90460
Kazagha <chrisg.local@gmail.com>
2018-04-21 14:52:16 +1000

    04_Argon_Assault: Move ship on X axis
    Move the ship using the xOffset across the horizontal axis 
    Prevent the ship from moving offscreen using Mathf.Clamp() method

--- 04_Argon_Assault/Assets/Scripts/PlayerController.cs
+++ 04_Argon_Assault/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : MonoBehaviour {
 
-    [Tooltip("In ms^-1")] [SerializeField] float xMoveSpeed = 3f;
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 3f;
+    [Tooltip("Max position in meters")][SerializeField] float xPositionMax = 4f;
 
 	// Use this for initialization
 	void Start () {
@@ -20,7 +21,14 @@ public class PlayerController : MonoBehaviour {
 
         // Determine the offset for the current frame 
         // Throw * Move Speed * Delta Time
-        float xOffset = xThrow * xMoveSpeed * Time.deltaTime;
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
         print(xOffset);
+
+        // Calculate the new X position 
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        // Prevent the player going offscreen 
+        float clampedXPos = Mathf.Clamp(rawNewXPos, -xPositionMax, xPositionMax);
+        // Move the ship to the new X position 
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
 	}
 }
