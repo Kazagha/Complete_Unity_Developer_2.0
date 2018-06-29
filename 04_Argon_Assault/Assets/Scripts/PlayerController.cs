b0cb48f000ffd6d50d5933c5dad3b6f0f3df77f5
Unknown <chrisg.local@gmail.com>
2018-06-30 07:57:22 +1000

    Argon_Assault: Add ship pitch, yaw and roll
    Depending on the position on screen and the x,y throw set the pitch, yaw and roll of the player ship

--- 04_Argon_Assault/Assets/Scripts/PlayerController.cs
+++ 04_Argon_Assault/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,17 @@ using UnityStandardAssets.CrossPlatformInput;
 public class PlayerController : MonoBehaviour {
 
     [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 7f;
-    [Tooltip("Max X position in meters")][SerializeField] float xPositionMax = 4f;
-    [Tooltip("Max Y position in meters")] [SerializeField] float yPositionMax = 2.1f;
+    [Tooltip("Max X position in meters")][SerializeField] float xPositionMax = 4.5f;
+    [Tooltip("Max Y position in meters")] [SerializeField] float yPositionMax = 2.5f;
+
+    [Tooltip("Pitch factor")] [SerializeField] float PositionPitchFactor = -5f;
+    [Tooltip("Control pitch factor")] [SerializeField] float ControlPitchFactor = -15f;
+    
+    [Tooltip("Position yaw factor")] [SerializeField] float PositionYawFactor = (25f / 4f);
+
+    [Tooltip("Control roll factor")] [SerializeField] float ControlRollFactor = -25f;
+
+    float xThrow, yThrow;
 
     // Use this for initialization
     void Start () {
@@ -17,10 +26,30 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = (this.transform.localPosition.y * PositionPitchFactor);
+        float pitchDueToThrow = (yThrow * ControlPitchFactor);
+        float pitch = pitchDueToPosition + pitchDueToThrow;
+
+        float roll = xThrow * ControlRollFactor;
+
+        float yaw = this.transform.localPosition.x * PositionYawFactor;
+
+        // Set the Pitch, Yaw and Roll
+        transform.localRotation = Quaternion.Euler(new Vector3(pitch, yaw, roll));
+    }
+
+    private void ProcessTranslation()
+    {
         // Fetch the 'Horizontal' and 'Vertical' input
         // Integer between -1 and 1
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");        
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
         MovePlayer(xThrow, yThrow);
     }
